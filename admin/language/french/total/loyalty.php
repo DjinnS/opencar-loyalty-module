d4c4cf385193973dc00aed7bdd49e1696ae8ea99
djinns <djinns@chninkel.net>
2011-10-07 21:44:47 +0200

    Admin module

---
+++ admin/language/french/total/loyalty.php
@@ -0,0 +1,16 @@
+<?php
+$_['text_loyalty']		= 'Carte de fid&eacute;lit&eacute;';
+// Heading
+$_['heading_title']		= 'Carte de fid&eacute;lit&eacute;';
+
+// Text
+$_['text_total']		= 'Point gagn&eacute; lors d\'une commande';
+$_['text_success']		= 'F&eacute;licitations, vous avez modifi&eacute; le <b>Carte de fid&eacute;lit&eacute;</b> avec succ&egrave;s !';
+
+// Entry
+$_['entry_status']		= '&Eacute;tat :';
+$_['entry_sort_order']	= 'Classement :';
+
+// Error
+$_['error_permission']	= 'Attention, vous n&#8217;avez pas la permission de modifier le <b>Carte de fid&eacute;lit&eacute;</b> !';
+?>
