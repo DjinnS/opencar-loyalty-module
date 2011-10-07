6e60253657de8bc72d1cac1f36d2feada299216f
djinns <djinns@chninkel.net>
2011-10-07 21:33:30 +0200

    Admin module, menu integration and frontend (cart) integration

---
+++ catalog/language/french/total/loyalty.php
@@ -0,0 +1,9 @@
+<?php
+/*
+* Author  : DjinnS - djinns@chninkel.net
+* 
+* License : GPL v2 (http://www.gnu.org/licenses/gpl-2.0.html)
+*/
+$_['text_loyalty']			= 'Point de fid&eacute;lit&eacute; gagn&eacute;';
+$_['text_loyalties']		= 'Points de fid&eacute;lit&eacute; gagn&eacute;s';
+?>
