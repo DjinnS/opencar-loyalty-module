69f8c435ceccf07547074820eb23e66c0d2115be
djinns <djinns@chninkel.net>
2011-11-13 16:10:25 +0100

    Add order status to admin module

---
+++ admin/language/french/sale/loyalty.php
@@ -0,0 +1,29 @@
+<?php 
+
+/*
+* Author  : DjinnS - djinns@chninkel.net
+* 
+* License : GPL v2 (http://www.gnu.org/licenses/gpl-2.0.html)
+*/
+
+// Heading
+$_['heading_title']       			= 'Carte de fid&eacute;lit&eacute;';
+
+// Text
+$_['loyalty_rate']         			= 'Ratio à appliquer';
+$_['loyalty_rate_explain'] 			= 'Le ratio indique le nombre de point gagn&eacute; sur le total du panier';
+$_['loyalty_threshold']    			= 'Seuil pour une reduction';
+$_['loyalty_threshold_explain']    	= 'Nombre de point à atteindre pour gagner un bon de r&eacute;duction';
+$_['loyalty_gain']         			= 'Valeur de la r&eacute;duction';
+$_['loyalty_gain_explain'] 			= 'Valeur du bon de r&eacute;duction gagn&eacute; une fois le seuil atteint';
+$_['loyalty_voucher']				= 'Bon cadeau';
+$_['loyalty_voucher_explain']		= 'Type de bon cadeau associ&eacute; à la carte de fid&eacute;lit&eacute;';
+$_['loyalty_order_status']			= 'Quand créditeur les points';
+$_['loyalty_order_status_explain']	= 'A quel moment status cr&eacutediter les points fid&eacute;lit&eacute;';
+
+// Error & success
+$_['text_success']					= 'Success: You have modified loyalty !';
+$_['error_rate']					= 'Le ratio doit être > 0 !';
+$_['error_threshold']				= 'Le seuil doit être > 0 !';
+$_['error_gain']					= 'Le gain doit être > 0 !';
+?>
